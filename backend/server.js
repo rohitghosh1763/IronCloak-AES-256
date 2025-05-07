506baa7062743ed9c966e0360749947b8c532b8d
Rohit Ghosh <rohitmca23-24@aec.ac.in>
2025-05-07 10:59:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const dbName = "ironcloak";
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
 );
